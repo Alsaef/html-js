de1dae0674920a8e3731d8d284537a0675650077
Alsaef <saef.ratul@gmail.com>
2025-08-28 12:11:47 +0600

    dev

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("I am from assignment 5")
+let coinsText=document.getElementById('coins')
+
+let callBtn=document.querySelectorAll('.callBtn')
+
+let coins=parseInt(coinsText.innerText)
+// let cardTarget=document.querySelectorAll('card')
+
+
+
+
+callBtn.forEach(btn=>{
+console.log(btn);
+  btn.addEventListener('click',function(){
+    console.log('dddd');
+
+      let conisSet= coinsText
+
+     conisSet.innerText =coins-=3
+
+       alert('history counted')
+
+
+       
+      
+  })
+
+})
+
+
